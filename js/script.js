856d43929ff9c8888342a5c374a67ad4e964532d
Rudra <chaudhary66667@gmail.com>
2022-07-11 09:52:30 +0530

    +header_complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-const headerForground = document.querySelector(".header__foreground");
-const headerBackground = document.querySelector(".header__background");
-const headerLine = document.querySelector(".header__line");
+const navigation = document.querySelector(".navigation");
+const openNavigationBtn = document.querySelector(".header__label");
 const body = document.querySelector("body");
 
-headerForground.addEventListener("click", function () {
-  headerBackground.classList.toggle("scaled");
+openNavigationBtn.addEventListener("click", function () {
+  navigation.classList.toggle("hidden");
+});
+
+// Smooth scrolling
+const navigationBox = document.querySelector(".navigation__nav");
+const checkBox = document.querySelector(".header__checkbox");
+
+const scrollIntoSection = function () {
+  // binded this to an actual value to use it as an arguement
+  document.querySelector(this).scrollIntoView({ behavior: "smooth" });
+};
+
+navigationBox.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("navigation__link")) {
+    const id = e.target.getAttribute("href");
+    // uncheck the checkbox
+    checkBox.checked = false;
+    // add hidden class
+    navigation.classList.add("hidden");
+    // scroll intoView
+    setTimeout(scrollIntoSection.bind(id), 1000);
+  }
 });
